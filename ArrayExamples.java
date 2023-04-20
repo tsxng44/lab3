7aa2ac6930d978ea1377eadc9c53e75678bd4641
tsxng44 <antseng@ucsd.edu>
2023-04-20 11:01:50 -0700

    new code

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,19 +4,21 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
+    int[] Array2 = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+      Array2[i] = arr[arr.length - i - 1];
     }
+    arr = Array2;
   }
 
   // Returns a *new* array with all the elements of the input array in reversed
   // order
   static int[] reversed(int[] arr) {
-    int[] newArray = new int[arr.length];
+    int[] Array2 = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      Array2[i] = arr[arr.length - i - 1];
     }
-    return arr;
+    return Array2;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,24 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlaceTwo() {
+    int[] input1 = {1, 2, 3, 4, 5, 6, 7, 8};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{8, 7, 6, 5, 4, 3, 2, 1}, input1);
+  }
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+    @Test
+  public void testReversedTwo() {
+    int[] input1 = {9, 7, 5, 3, 2, 1};
+    assertArrayEquals(new int[]{1, 2, 3, 5, 7, 9}, ArrayExamples.reversed(input1));
+
   }
 }
